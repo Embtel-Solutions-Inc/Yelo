10c8934a5e96386ee35003c19a73ef0d8336e549
Trupal <38033371+palvsv@users.noreply.github.com>
2021-06-29 17:46:38 +0530

    Fix customer module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,19 @@ import { ShopCategoryComponent } from './components/site/shop-category/shop-cate
 import { ProductComponent } from './components/site/product/product.component';
 import { WishlistComponent } from './components/site/wishlist/wishlist.component';
 import { CartComponent } from './components/site/cart/cart.component';
+import { ContactComponent } from './components/site/contact/contact.component';
+import { AboutComponent } from './components/site/about/about.component';
+import { FaqComponent } from './components/site/faq/faq.component';
+import { CheckoutComponent } from './components/site/checkout/checkout.component';
+import { TermsComponent } from './components/site/terms/terms.component';
+import { PolicyComponent } from './components/site/policy/policy.component';
+import { VendorComponent } from './components/site/vendor/vendor.component';
+import { LoginComponent } from './components/site/login/login.component';
+import { VendorRegistrationComponent } from './components/site/vendor-registration/vendor-registration.component';
+import { BlogsComponent } from './components/site/blogs/blogs.component';
+import { SingleBlogComponent } from './components/site/blogs/single-blog/single-blog.component';
+import { OrderTrackingComponent } from './components/site/order-tracking/order-tracking.component';
+import { StoreListingComponent } from './components/site/store-listing/store-listing.component';
 
 const appRoutes: Routes = [
 	{ path: 'home', component: HomeComponent},
@@ -21,6 +34,19 @@ const appRoutes: Routes = [
 	{ path: 'product', component: ProductComponent},
 	{ path: 'wishlist', component: WishlistComponent},
 	{ path: 'cart', component: CartComponent},
+	{ path: 'contact', component: ContactComponent},
+	{ path: 'about', component: AboutComponent},
+	{ path: 'faqs', component: FaqComponent},
+	{ path: 'checkout', component: CheckoutComponent},
+	{ path: 'terms', component: TermsComponent},
+	{ path: 'policy', component: PolicyComponent},
+	{ path: 'merchant', component: VendorComponent},
+	{ path: 'login', component: LoginComponent},
+	{ path: 'vendor-registration', component: VendorRegistrationComponent},
+	{ path: 'blogs', component: BlogsComponent},
+	{ path: 'single-post', component: SingleBlogComponent},
+	{ path: 'order-tracking', component: OrderTrackingComponent},
+	{ path: 'store-list', component: StoreListingComponent},
 	{ path: '',
 	  redirectTo: 'home',
 	  pathMatch: 'full'
@@ -38,7 +64,20 @@ const appRoutes: Routes = [
 	ShopCategoryComponent,
 	ProductComponent,
 	WishlistComponent,
-	CartComponent
+	CartComponent,
+ ContactComponent,
+ AboutComponent,
+ FaqComponent,
+ CheckoutComponent,
+ TermsComponent,
+ PolicyComponent,
+ VendorComponent,
+ LoginComponent,
+ VendorRegistrationComponent,
+ BlogsComponent,
+ SingleBlogComponent,
+ OrderTrackingComponent,
+ StoreListingComponent
   ],
   imports: [
 	BrowserModule,
